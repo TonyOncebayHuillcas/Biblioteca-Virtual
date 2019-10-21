e64d11034494938ee8ec02d265c55f8d04d6dd6b
Tony Oncebay Huillcas <tony.oncebay.huillcas@gmail.com>
2019-10-21 11:46:01 -0500

    Create Class and view of Sign in and move activities to folder Activity

--- BibliotecaVirtual/app/src/main/java/com/example/bibliotecavirtual/Activitys/Activitys/LoginActivity.java
+++ BibliotecaVirtual/app/src/main/java/com/example/bibliotecavirtual/Activitys/Activitys/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.bibliotecavirtual.Activitys;
+package com.example.bibliotecavirtual.Activitys.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ BibliotecaVirtual/app/src/main/java/com/example/bibliotecavirtual/Activitys/Activitys/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.bibliotecavirtual.Activitys.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bibliotecavirtual.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
